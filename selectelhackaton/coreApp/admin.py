456251bff5fb2adda9271f772e78adcce846c86b
mar4elkin <googlcraftlol@gmail.com>
2019-10-12 17:57:21 +0300

    feat(coreApp): вывод sqad'ов и заданий в админку

--- selectelhackaton/coreApp/admin.py
+++ selectelhackaton/coreApp/admin.py
@@ -3,7 +3,7 @@ from django.contrib             import admin
 from import_export              import resources
 from import_export.admin        import ImportExportModelAdmin
 
-from selectelhackaton.coreApp.models import Order
+from selectelhackaton.coreApp.models import Order, Task, Squad
 # Register your models here.
 
 # admin.site.register(Order)
@@ -11,6 +11,14 @@ class OrderResource(resources.ModelResource):
     # https://django-import-export.readthedocs.io/en/latest/getting_started.html
     class Meta:
         model = Order
+
+class TaskResource(resources.ModelResource):
+    class Meta:
+        model = Task
+
+class SquadResource(resources.ModelResource):
+    class Meta:
+        model = Squad
 
 @admin.register(Order)
 class OrderAdmin(ImportExportModelAdmin):
@@ -22,3 +30,39 @@ class OrderAdmin(ImportExportModelAdmin):
     search_fields = ('title',)
 
     ordering = ('created_date',)
+
+@admin.register(Task)
+class TaskAdmin(ImportExportModelAdmin):
+    '''Admin View for Task'''
+
+    resource_class = TaskResource
+
+    list_display = (
+        'title', 
+        'created_date', 
+        'description', 
+        'deadline', 
+        'tags', 
+        'updated_data', 
+        'author', 
+        'is_internal',
+        'status'
+        )
+
+    list_filter = ('created_date', 'is_internal')
+
+    search_fields = ('title',)
+
+    ordering = ('created_date',)
+
+
+@admin.register(Squad)
+class SquadAdmin(ImportExportModelAdmin):
+    '''Admin View for Squad'''
+    resource_class = SquadResource
+
+    list_display = ('id','description','teammate_str')
+    list_filter = ('id',)
+    search_fields = ['id',]
+
+    ordering = ('id',)
