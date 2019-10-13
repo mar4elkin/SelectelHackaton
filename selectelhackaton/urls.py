89f9a8ec3adad37472ae334ff57d0b811652dde2
mar4elkin <googlcraftlol@gmail.com>
2019-10-13 05:48:09 +0300

    fix(templates): админ панель в ropdown

--- selectelhackaton/urls.py
+++ selectelhackaton/urls.py
@@ -30,5 +30,5 @@ urlpatterns = [
     path('core/', include('selectelhackaton.coreApp.urls')), 
 
     # Usual Django admin
-    path('admin/', admin.site.urls, name='adminp'),
+    path('admin/', admin.site.urls, ),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
